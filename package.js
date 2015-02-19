89a2a74356967a44345716e8f7f08fccb853d49b
Juan Esteban Bernabó <juan@germinadora.com>
2015-02-19 03:18:24 -0200

    Fixed problem with export of symbols

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
   summary: "This package intends to simplify the integration with a Meteor app and the Iugu payment API",
-  version: "0.0.2",
+  version: "0.0.3",
   name: "germinadora:iugu",
   git: "https://github.com/Germinadora/meteor-iugu.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom('0.9.0');
+  api.export('Iugu', 'server');
+  api.export('Iugu', 'client');
   api.add_files('server/iugu_api.coffee', ['server']);
   api.add_files('client/iugu.js', ['client']);
 });
